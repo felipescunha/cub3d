48bdbfc8161c2cbbe3ee973c6580bf0b18f61006
felipescunha <fcunha.ti@gmail.com>
2023-07-01 21:50:52 -0300

    check_coordinate_no

--- src/validations/check_coordinate_no.c
+++ src/validations/check_coordinate_no.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_coordinate_no.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: learn <learn@student.42.fr>                +#+  +:+       +#+        */
+/*   By: fecunha <fecunha@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 21:00:22 by learn             #+#    #+#             */
-/*   Updated: 2023/07/01 00:40:20 by learn            ###   ########.fr       */
+/*   Updated: 2023/07/01 21:40:43 by fecunha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,26 @@
 
 void check_coordinate_no(t_cub3d *cub3d)
 {
-
 	int		i;
-	
-	i = 0;
+	int		fd;
+	char	*extension;
 
+	i = 0;
 	while (i < cub3d->read_lines)
 	{
 		if(ft_strncmp(cub3d->file[i], "NO", 2) == 0)
 		{
-        	ft_printf("%s\n", cub3d->file[i]);
+			if(ft_strchr(cub3d->file[i], '/'))
+			{
+				fd = open(ft_strchr(cub3d->file[i], '.'), O_RDONLY);
+				if(fd < 0)
+					printf("Error in texture file!\n----------\n");
+				extension = ft_strrchr(cub3d->file[i], '.');
+				if (!extension)
+					print_error("The map not found!\n");
+				if (ft_strncmp(extension, ".xpm", 5))
+					print_error("The file needed .xpm extension\n");
+			}
 		}
 		i++;
 	}
